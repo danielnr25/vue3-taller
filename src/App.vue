4d0e4490da62259cad76b4d4bd08137b0b380e2d
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-06-07 22:00:13 -0500

    feat:initial

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,62 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 import { db } from './firebase'
 import { collection, getDocs, onSnapshot, addDoc,deleteDoc,doc,updateDoc,query,orderBy,limit } from "firebase/firestore";
+
+
+const collectionRef = collection(db,'todos');
+const todoCollectionQuery = query(collectionRef,orderBy("date","desc"));
+
 const todos = ref([]);
 
+onMounted(() =>{
+   onSnapshot(todoCollectionQuery, (querySnapshot) =>{ //onSnapshot
+      const fbTodos = [];
+      querySnapshot.forEach((doc)=>{
+         const todo = {
+            id: doc.id,
+            nombre: doc.data().nombre,
+            descripcion: doc.data().descripcion,
+            completado: doc.data().completado,
+            date: doc.data().date
+         };
+         fbTodos.push(todo);
+      });
+      todos.value = fbTodos;
+   })
+});
 
 const newTodo = ref({
    nombre: '',
    descripcion: '',
-   completado: false
+   completado: false,
+
 });
 
 const addTodo = async () =>{
-   
+   const fechaActual = new Date();
+   const dateformate = fechaActual.toISOString().slice(0, 10) + ' ' + fechaActual.toLocaleTimeString('en-GB');
+
+   addDoc(collectionRef,{
+      nombre: newTodo.value.nombre,
+      descripcion: newTodo.value.descripcion,
+      completado: newTodo.value.completado,
+      date:dateformate
+   });
+   newTodo.value.nombre = '';
+   newTodo.value.descripcion = '';
 }
 
+const deleteTodo = (id) =>{
+   deleteDoc(doc(collectionRef,id));
+}
+
+const udpateTodo = (id) =>{
+   const index = todos.value.findIndex(todo => todo.id === id);
+   updateDoc(doc(collectionRef,id),{
+      completado: !todos.value[index].completado
+   })
+}
 
 </script>
 
@@ -51,6 +93,7 @@ const addTodo = async () =>{
                </div>
                <div>
                   <button 
+                  :disabled="!newTodo.nombre || !newTodo.descripcion"
                   type="submit"
                   class="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                   >
@@ -63,26 +106,42 @@ const addTodo = async () =>{
       </div>
 
       <!-- Listado  -->
-      <div class="bg-gray-100 py-14 sm:py-14">
+      <div class="bg-white py-14 sm:py-14">
          <div class="mx-auto max-w-7xl px-6 lg:px-8">
            <div class="mx-auto max-w-2xl lg:mx-0">
-               <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Listado</h2>
+               <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Listado de Tareas</h2>
             </div>
             <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-8 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-               <article  class="flex max-w-xl flex-col items-start justify-between shadow-xl p-6 rounded-lg transition-colors duration-200 ease-in-out transform">
+               <article  
+               v-bind:key="todo.id"
+               v-bind:class="{'bg-green-100': todo.completado}"
+               class="flex max-w-xl flex-col items-start justify-between shadow-xl p-6 rounded-lg transition-colors duration-200 ease-in-out transform"
+               v-for="todo in todos"
+               >
                   <div class="group relative">
                      <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                        <p class="block transition-colors duration-200"> Mi primer listado</p>
+                        <p 
+                           class="block transition-colors duration-200"
+                           :class="{'line-through': todo.completado}"
+                           >
+                           {{ todo.nombre }} - <span class="text-xs">{{todo.date}}</span>
+                        </p>
                      </h3>
-                     <p class="mt-5 text-sm leading-6 text-gray-600">Descripción del listado</p>
+                     <p class="mt-5 text-sm leading-6 text-gray-600">
+                        {{ todo.descripcion }}
+
+                     </p>
                   </div>
-                  <div class="relative mt-8 flex items-center justify-between gap-x-28">
+                  <div class="relative mt-8 flex items-center justify-between gap-x-36">
                      <div class="flex items-center gap-x-2">
                         <input type="checkbox"
+                        @click="udpateTodo(todo.id)"
+                        v-model="todo.completado"
                         class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"/>
                         <span class="text-sm font-medium leading-6 text-gray-900">Completado</span>
                      </div>
                      <button
+                        @click="deleteTodo(todo.id)"
                         type="submit"
                         class="flex justify-center w-24 rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                      >
