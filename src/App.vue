c7cd26c032bc4f98c8d31c12b4294d8883f51252
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-06-07 20:08:45 -0500

    feat:initial

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,10 @@
                   </div>
                </div>
                <div>
-                  <button>
+                  <button 
+                  type="submit"
+                  class="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                  >
                      Guardar
                   </button>
                </div>
@@ -39,18 +42,18 @@
       </div>
 
       <!-- Listado  -->
-      <div>
-         <div>
-            <div>
-               <h2>Listado</h2>
+      <div class="bg-gray-100 py-14 sm:py-14">
+         <div class="mx-auto max-w-7xl px-6 lg:px-8">
+           <div class="mx-auto max-w-2xl lg:mx-0">
+               <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Listado</h2>
             </div>
-            <div>
-               <article>
-                  <div>
-                     <h3>
-                        <p>Mi primer listado</p>
+            <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-8 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+               <article  class="flex max-w-xl flex-col items-start justify-between shadow-xl p-6 rounded-lg transition-colors duration-200 ease-in-out transform">
+                  <div class="group relative">
+                     <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                        <p class="block transition-colors duration-200"> Mi primer listado</p>
                      </h3>
-                     <p>Descripción del listado</p>
+                     <p class="mt-5 text-sm leading-6 text-gray-600">Descripción del listado</p>
                   </div>
                </article>
             </div>
