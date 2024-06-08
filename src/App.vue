9cb7e37378eeca9fbd0a2ef507bf10e263c8784c
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-06-07 21:01:12 -0500

    feat:initial

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,14 @@ const todos = ref([]);
 
 const newTodo = ref({
    nombre: '',
-   descripcion: ''
+   descripcion: '',
+   completado: false
 });
+
+const addTodo = () =>{
+   console.log(newTodo.value);
+}
+
 
 </script>
 
@@ -18,7 +24,7 @@ const newTodo = ref({
       </div>
       <div class="flex min-h-full flex-col justify-center px-6 py-2 lg:px-8">
          <div class="mt-2 sm:mx-auto sm:w-full sm:max-w-sm shadow-lg px-4 py-4 rounded-md">
-            <form class="space-y-6" action="">
+            <form class="space-y-6" @submit.prevent="addTodo()">
                <div>
                   <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Nombre</label>
                   <div>
@@ -26,6 +32,7 @@ const newTodo = ref({
                         type="text" 
                         name="name" 
                         id="nombre" 
+                        v-model="newTodo.nombre"
                         class="block w-full rounded-md border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                   </div>
                </div>
@@ -36,6 +43,7 @@ const newTodo = ref({
                     <textarea 
                     name="descripcion"
                     id="descripcion"
+                    v-model="newTodo.descripcion"
                     rows="3" 
                     class="block w-full rounded-md px-4 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                   </div>
@@ -66,6 +74,18 @@ const newTodo = ref({
                         <p class="block transition-colors duration-200"> Mi primer listado</p>
                      </h3>
                      <p class="mt-5 text-sm leading-6 text-gray-600">Descripción del listado</p>
+                  </div>
+                  <div class="relative mt-8 flex items-center justify-between gap-x-28">
+                     <div class="flex items-center gap-x-2">
+                        <input type="checkbox"
+                        class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"/>
+                        <span class="text-sm font-medium leading-6 text-gray-900">Completado</span>
+                     </div>
+                     <button
+                        type="submit"
+                        class="flex justify-center w-24 rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                     >
+                     </button>
                   </div>
                </article>
             </div>
---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3-AgQjuEV7cgCkrtLV4utNPOLlGjOHW4",
+  authDomain: "appvue-taller.firebaseapp.com",
+  projectId: "appvue-taller",
+  storageBucket: "appvue-taller.appspot.com",
+  messagingSenderId: "934314237664",
+  appId: "1:934314237664:web:ec1d76cf9ce899702bfa85"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
